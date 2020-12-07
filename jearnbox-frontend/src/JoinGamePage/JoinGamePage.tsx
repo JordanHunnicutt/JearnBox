0a693d9a98e540b1e05572a2856ccf6463072f2a
JordanHunnicutt <jordanhunnicutt97@gmail.com>
2020-12-07 16:39:43 -0500

    initial push with joinGamePage component

--- jearnbox-frontend/src/JoinGamePage/JoinGamePage.tsx
+++ jearnbox-frontend/src/JoinGamePage/JoinGamePage.tsx
@@ -1,5 +1,6 @@
 import React, { Fragment } from 'react'
 import {Form, FormGroup, Input, Label} from 'reactstrap'
+import './JoinGamePage.css'
 
 export const JoinGamePage:React.FC<any> = (props:any) => {
 
@@ -9,12 +10,16 @@ export const JoinGamePage:React.FC<any> = (props:any) => {
             <h1>JearnBox</h1>
             <Form>
                 <FormGroup>
-                    <Label for="userName">Username:</Label>
+                    <Label for="userName">Username: </Label>
+                    <br />
                     <Input type="text" name="userName" id="userName" placeholder="12 char max" maxLength={12} />
+                    <br />
                 </FormGroup>
                 <FormGroup>
-                    <Label for="roomCode">Room Code:</Label>
+                    <Label for="roomCode">Room Code: </Label>
+                    <br />
                     <Input type="text" name="roomCode" id="roomCode" placeholder="6 char" maxLength={6} />
+                    <br />
                 </FormGroup>
                 <Input type="submit" name="submit" id="submit" value="Enter Room" />
             </Form>
