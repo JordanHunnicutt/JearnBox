268e37b4576b2e7a0c1307f388239daf0178f9a8
JordanHunnicutt <jordanhunnicutt97@gmail.com>
2020-12-08 13:57:21 -0500

    joinGamePage functionality done besides axios

--- jearnbox-frontend/src/JoinGamePage/JoinGamePage.tsx
+++ jearnbox-frontend/src/JoinGamePage/JoinGamePage.tsx
@@ -1,18 +1,58 @@
+import Axios from 'axios';
 import React, { Fragment } from 'react'
 import {Form, FormGroup, Input, Label} from 'reactstrap'
+import {Redirect} from 'react-router-dom'
 import './JoinGamePage.css'
 
 export const JoinGamePage:React.FC<any> = (props:any) => {
 
+    const [errMess, setErrMess] = React.useState("");
+    const [gameJoined, setGameJoined] = React.useState(false);
+
+
+    const enterRoom = (event:any) => {
+        event?.preventDefault();
+        console.log("Button clicked")
+        const form = event.currentTarget.parentElement;
+        const userName = form.firstChild.childNodes[2].value;
+        const roomCode = form.childNodes[1].childNodes[2].value;
+        
+        if(userName == ""){
+            setErrMess("Please input a valid username");
+            return;
+        } else if(roomCode.length < 6){
+            setErrMess("Please input a valid room code");
+            return;
+        } else{
+            setErrMess("");
+        }
+
+        let result;
+
+        try{
+            result = "someIP";
+            //const result = await Axios.get("url/{roomCode}");
+        } catch(error){
+            setErrMess("Something went wrong trying to connect to the server");
+            return;
+        }
+
+        setGameJoined(true);
+
+    }
+
 
     return (
         <>
+            {gameJoined ? /*<Redirect to="/gamePage" />*/ console.log('game joined')
+            :
+            <>
             <h1>JearnBox</h1>
             <Form>
                 <FormGroup>
                     <Label for="userName">Username: </Label>
                     <br />
-                    <Input type="text" name="userName" id="userName" placeholder="12 char max" maxLength={12} />
+                    <input type="text" name="userName" id="userName" placeholder="12 char max" maxLength={12} />
                     <br />
                 </FormGroup>
                 <FormGroup>
@@ -21,8 +61,11 @@ export const JoinGamePage:React.FC<any> = (props:any) => {
                     <Input type="text" name="roomCode" id="roomCode" placeholder="6 char" maxLength={6} />
                     <br />
                 </FormGroup>
-                <Input type="submit" name="submit" id="submit" value="Enter Room" />
+                <Input type="submit" name="submit" id="submit" value="Enter Room" onClick={enterRoom}/>
             </Form>
+            <p className="errMessClass">{errMess}</p>
+            </>
+    }
         </>
     )
 
